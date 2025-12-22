e5937752e48b3e72e05c384d5e096d4f44b90a97
sangram behera <sangrambeheraa007@gmail.com>
2025-12-22 21:01:58 +0530

    server file fix

--- FRONTEND/src/services/api.js
+++ FRONTEND/src/services/api.js
@@ -1,7 +1,7 @@
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1";
 
-
+console.log("url ", import.meta.env.VITE_API_BASE_URL);
 class ApiClient {
   constructor() {
     this.baseUrl = API_BASE_URL;
