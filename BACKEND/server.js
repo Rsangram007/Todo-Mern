60fea6fdba47117ba07dac576c61639011eb9e01
sangram behera <sangrambeheraa007@gmail.com>
2025-12-22 20:54:13 +0530

    backend url added

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -15,14 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Enable CORS with specific options for frontend
-const corsOptions = {
-  origin: [process.env.FRONTEND_URL || "http://localhost:8080"],
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors("*"));
 
 // Handle preflight requests explicitly
 app.options("*", cors(corsOptions));
